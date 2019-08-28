8f575045d365994fd26951f8bd7abb1f76bba1f5
lishuo <45788736+MonsterMz24@users.noreply.github.com>
2019-08-28 17:21:37 +0800

    Add
    添加项目

---
+++ Creative/Creative/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Creative.Models;
+
+namespace Creative.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// 404主页
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 魔方
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 2048
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 音乐
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult musics()
+        {
+            return View();
+        }
+        
+        /// <summary>
+        /// 时间
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Clock()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
